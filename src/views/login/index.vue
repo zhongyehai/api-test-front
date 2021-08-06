8f73f3073ac70aec9cb2f37765a9bcfe00a50148
zhongyehai <zyh505242zyh>
2021-08-06 15:35:23 +0800

    去除默认值

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -81,8 +81,8 @@ export default {
     }
     return {
       loginForm: {
-        account: 'admin',
-        password: '111111'
+        account: '',
+        password: ''
       },
       loginRules: {
         account: [{required: true, trigger: 'blur', validator: validateAccount}],
